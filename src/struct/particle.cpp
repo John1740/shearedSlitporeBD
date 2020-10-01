134687b5acd91f130bc3cbd52a3ec86bcdc44197
m_huelsberg <m.huelsberg@tu-berlin.de>
2020-10-01 14:02:00 +0200

    reformatted particle.cpp

--- src/struct/particle.cpp
+++ src/struct/particle.cpp
@@ -8,7 +8,7 @@ PARTICLE::PARTICLE(){
     index = 0;
 }
 
-PARTICLE::PARTICLE(const REAL_C &positionIN){
+PARTICLE::PARTICLE(const REAL_C& positionIN){
     position = positionIN;
     boxPosition = REAL_C(0.);
     diameter = 1;
@@ -16,7 +16,7 @@ PARTICLE::PARTICLE(const REAL_C &positionIN){
     index = 0;
 }
 
-PARTICLE::PARTICLE(const PARTICLE &other){
+PARTICLE::PARTICLE(const PARTICLE& other){
     position = other.position;
     boxPosition = other.boxPosition;
     diameter = other.diameter;
@@ -28,34 +28,34 @@ REAL_C PARTICLE::getPosition() const{
     return REAL_C(position.x, position.y, position.z);
 }
 
-double PARTICLE::distanceTo(PARTICLE &other){
+double PARTICLE::distanceTo(PARTICLE& other){
     REAL_C difference;
     difference = position - other.position; //change to boxPosition?
     return difference.abs();
 }
 
-double PARTICLE::distanceTo(REAL_C &positionIN){
+double PARTICLE::distanceTo(REAL_C& positionIN){
     REAL_C difference;
     difference = position - positionIN;  //change to boxPosition?
     return difference.abs();
 }
 
 //needs the simulation box information
-double PARTICLE::boxDistanceTo(PARTICLE &other, BOX_GEOMETRY& boxGeom){
+double PARTICLE::boxDistanceTo(PARTICLE& other, BOX_GEOMETRY& boxGeom){
     REAL_C difference;
     difference = boxPosition - other.boxPosition;
     difference = boxGeom.convertToBoxPosition(difference);
     return difference.abs();
 }
 
-double PARTICLE::boxDistanceTo(REAL_C &positionIN, BOX_GEOMETRY& boxGeom){
+double PARTICLE::boxDistanceTo(REAL_C& positionIN, BOX_GEOMETRY& boxGeom){
     REAL_C difference;
     difference = boxPosition - positionIN;
     difference = boxGeom.convertToBoxPosition(difference);
     return difference.abs();
 }
 
-void PARTICLE::setBoxPosition(BOX_GEOMETRY &boxGeom){
+void PARTICLE::setBoxPosition(BOX_GEOMETRY& boxGeom){
     boxPosition = boxGeom.convertToBoxPosition(position);
 }
 

7611af8a98c843a221da96b206d6c5bd4506ee55
m_huelsberg <m.huelsberg@tu-berlin.de>
2020-10-01 13:47:29 +0200

    reformatted system_interface.h

--- src/interfaces/system_interface.h
+++ src/interfaces/system_interface.h
@@ -7,7 +7,7 @@
 
 class SYSTEM_INTERFACE{
 public:
-    virtual void calculateInteractionForce(int i, int j)  = 0;
+    virtual void calculateInteractionForce(int i, int j) = 0;
     virtual void calculateExternalForce(int i) = 0;
 
     virtual BOX_GEOMETRY* simulationBox() = 0;

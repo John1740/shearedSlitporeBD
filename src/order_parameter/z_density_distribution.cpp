eb4cf864abe58666fee465525b6ca311cb95330a
Can Kahriman <kahriman@campus.tu-berlin.de>
2022-06-01 22:48:51 +0200

    Observable Density Distribution over Z first Implementation

--- src/order_parameter/z_density_distribution.cpp
+++ src/order_parameter/z_density_distribution.cpp
@@ -1,1 +1,81 @@
- 
+//
+// Created by
+//
+
+#include <boost/format.hpp>
+#include "z_density_distribution.h"
+
+namespace b = boost;
+
+Z_DENSITY_DISTRIBUTION::Z_DENSITY_DISTRIBUTION(){
+}
+
+Z_DENSITY_DISTRIBUTION::Z_DENSITY_DISTRIBUTION(CONFINED_BROWNIAN_PARTICLES& sys){
+    setup(sys, 10000);
+}
+
+Z_DENSITY_DISTRIBUTION::Z_DENSITY_DISTRIBUTION(CONFINED_BROWNIAN_PARTICLES& sys,double nOB){
+    setup(sys, nOB);
+}
+Z_DENSITY_DISTRIBUTION&
+Z_DENSITY_DISTRIBUTION::setup(CONFINED_BROWNIAN_PARTICLES& sys, double nob){
+    simBox = sys.getSimulationBox();
+    layers = LAYERS(simBox);
+    particle = sys.getParticleList();
+    return *this;
+}
+
+Z_DENSITY_DISTRIBUTION& Z_DENSITY_DISTRIBUTION::calculateZDensityDistribution(){
+    //int numberOfLayers = layers.getNumberOfLayers();
+    double currentZ;
+    //zDensityDistribution.clear();
+    //zDensityDistribution.resize(nob);
+    zDensityDistribution.assign(nob, double(0.));
+    zCoordinate.assign(nob, double(0.));
+    zrange = simBox.getDimensions().z;
+    db = nob / zrange;
+    dr = zrange / nob;
+    numberOfParitcles = particle.size();
+    for(int i = 0; i < numberOfParitcles; i++){
+        currentZ = particle[i].boxPosition.z;
+        int j = round((currentZ + zrange / 2) * db);
+        zDensityDistribution[j] += 1;
+        zCoordinate[i]=-zrange/2+dr*i;
+    }
+}
+
+Z_DENSITY_DISTRIBUTION& Z_DENSITY_DISTRIBUTION::print(string filename, bool overwrite, string header){
+    PRINTER printer(filename);
+    if(overwrite){
+        printer.reset();
+    }
+    if(header.size() > 0){
+        printer << string(3, printer.getComment()) << header << endl;
+    }
+    printer << *this;
+    return *this;
+}
+
+ostream& operator<<(ostream& os, const Z_DENSITY_DISTRIBUTION& zdis){
+    os << "first line: z coordinate [diameter]" << endl;
+    os << "second line: particle density at z [1/diameter]" << endl;
+
+    //data
+    for(int r = 0; r < zdis.nob; r++){
+        os << b::format("% 2.5f") % zdis.zCoordinate[r];
+        os << "\t";
+    }
+    os << "\n";
+
+    for(int r = 0; r < zdis.nob; r++){
+        os << b::format("% 2.5f") % zdis.zDensityDistribution[r];
+        os << "\t";
+    }
+    os << "\n";
+
+    return os;
+
+
+
+
+}
--- src/order_parameter/z_density_distribution.h
+++ src/order_parameter/z_density_distribution.h
@@ -1,1 +1,37 @@
- 
+//
+// Created by
+//
+
+#ifndef SHEAREDSLITPOREBD_Z_DENSITY_DISTRIBUTION_H
+#define SHEAREDSLITPOREBD_Z_DENSITY_DISTRIBUTION_H
+
+#include "../struct/layers.h"
+#include "../systems/confined_brownian_particles.h"
+
+class Z_DENSITY_DISTRIBUTION{
+private:
+    LAYERS layers;
+    SLIT_PORE_BOX simBox;
+    vector<CHARGED_PARTICLE> particle;
+    double nob;
+    double db;
+    double dr;
+    double zrange;
+    int numberOfParitcles;
+    vector<double> zCoordinate;
+    vector<double> zDensityDistribution;
+
+public:
+    Z_DENSITY_DISTRIBUTION();
+    Z_DENSITY_DISTRIBUTION(CONFINED_BROWNIAN_PARTICLES& sys);
+    Z_DENSITY_DISTRIBUTION(CONFINED_BROWNIAN_PARTICLES& sys, double nob);
+
+    Z_DENSITY_DISTRIBUTION& setup(CONFINED_BROWNIAN_PARTICLES& sys, double bob);
+    Z_DENSITY_DISTRIBUTION& calculateZDensityDistribution();
+
+    Z_DENSITY_DISTRIBUTION& print(string filename, bool overwrite=false, string header="");
+    friend ostream& operator<<(ostream& os, const  Z_DENSITY_DISTRIBUTION& zDistribution);
+
+    };
+
+#endif //SHEAREDSLITPOREBD_Z_DENSITY_DISTRIBUTION_H

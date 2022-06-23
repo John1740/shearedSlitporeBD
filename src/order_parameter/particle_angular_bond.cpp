3aaba16c83cd9856d4c9d13358db4dfe174b08ab
Can Kahriman <kahriman@campus.tu-berlin.de>
2022-06-23 18:28:01 +0200

    Files for feature to print Angular Bond Parameter of every particle to snapshot.out

---
+++ src/order_parameter/particle_angular_bond.cpp
@@ -0,0 +1,31 @@
+ 
+#include "particle_angular_bond.h"
+
+PARTICLE_ANGULAR_BOND::PARTICLE_ANGULAR_BOND(){
+}
+
+PARTICLE_ANGULAR_BOND::PARTICLE_ANGULAR_BOND(CONFINED_BROWNIAN_PARTICLES& sys){
+    setup(sys);
+}
+
+PARTICLE_ANGULAR_BOND& PARTICLE_ANGULAR_BOND::setup(CONFINED_BROWNIAN_PARTICLES& sys){
+    numberOfParticles = sys.getParticleList().size();
+    angularBond4.assign(numberOfParticles, double(0.));
+    angularBond6.assign(numberOfParticles, double(0.));
+    nearestNeighbors.assign(numberOfParticles, double(0.));
+    return *this;
+}
+
+PARTICLE_ANGULAR_BOND& PARTICLE_ANGULAR_BOND::calculateParticleAngularBond(CONFINED_BROWNIAN_PARTICLES& sys){
+    ANGULAR_BOND_PARAMETER ABP(sys,4);
+    NNRadius=ABP.getNextNeighborShellRadius();
+    for(int j = 0; j < numberOfParticles; j++){
+        ABP.setN(4);
+        angularBond4[j]=ABP.calculateForSingleParticle(j);
+        ABP.setN(6);
+        angularBond6[j]=ABP.calculateForSingleParticle(j);
+        nearestNeighbors[j]=ABP.getNearestNeighborIndices(j).size();
+
+    }
+    return *this;
+}
---
+++ src/order_parameter/particle_angular_bond.h
@@ -0,0 +1,24 @@
+#ifndef SHEAREDSLITPOREBD_PARTICLE_ANGULAR_BOND_H
+#define SHEAREDSLITPOREBD_PARTICLE_ANGULAR_BOND_H
+
+//#include "printer.h"
+#include "../systems/sheared_slitpore_system.h"
+#include "../order_parameter/angular_bond_parameter.h"
+
+class PARTICLE_ANGULAR_BOND{
+private:
+    int numberOfParticles;
+
+
+public:
+    double NNRadius;
+    vector<double> angularBond4;
+    vector<double> angularBond6;
+    vector<double> nearestNeighbors;
+    PARTICLE_ANGULAR_BOND();
+    PARTICLE_ANGULAR_BOND(CONFINED_BROWNIAN_PARTICLES& sysIn);
+    PARTICLE_ANGULAR_BOND& setup(CONFINED_BROWNIAN_PARTICLES& sysIn);
+    PARTICLE_ANGULAR_BOND& calculateParticleAngularBond(CONFINED_BROWNIAN_PARTICLES& sysIn);
+};
+
+#endif //SHEAREDSLITPOREBD_PARTICLE_ANGULAR_BOND_H
